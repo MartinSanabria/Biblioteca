b62192cd9fabb4b2cadf90cdf2c1594f01a5b7cf
RaulValencia04 <raulvalenciau@gmail.com>
2022-10-05 15:01:41 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Tecnicas/clases/puerto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Tecnicas.clases
+{
+	internal class puerto
+	{
+		private string pais, nombre, profundidad, tipoGrua,tipoAgua;
+		private double barcoCapacidad;
+
+
+		public puerto() { }
+		public puerto(string pais, string nombre, string profundidad, string tipoGrua, string tipoAgua, double barcoCapacidad)
+		{
+			Pais = pais;
+			Nombre = nombre;
+			Profundidad = profundidad;
+			TipoGrua = tipoGrua;
+			TipoAgua = tipoAgua;
+			BarcoCapacidad = barcoCapacidad;
+		}
+
+
+		//encapsulamiento a modificar
+		public string Pais { get => pais; set => pais = value; }
+		public string Nombre { get => nombre; set => nombre = value; }
+		public string Profundidad { get => profundidad; set => profundidad = value; }
+		public string TipoGrua { get => tipoGrua; set => tipoGrua = value; }
+		public string TipoAgua { get => tipoAgua; set => tipoAgua = value; }
+		public double BarcoCapacidad { get => barcoCapacidad; set => barcoCapacidad = value; }
+	}
+}
---
+++ Proyecto_Tecnicas/clases/zonaPesca.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Tecnicas.clases
+{
+	internal class zonaPesca : puerto
+	{
+		private string codigo,ubicacion,coordenadas,especies;
+		private DateTime uso;
+		public zonaPesca() { }
+
+		public zonaPesca(string codigo, string ubicacion, string coordenadas,
+			string especies, DateTime uso)
+		{
+			this.codigo = codigo;
+			this.ubicacion = ubicacion;
+			this.coordenadas = coordenadas;
+			this.especies = especies;
+			this.uso = uso;
+		}
+
+		public zonaPesca(string pais, string nombre, string profundidad,
+			string tipoGrua, string tipoAgua, double barcoCapacidad,
+			string codigo,string ubicacion,string coordenadas,string especies
+			,DateTime uso): 
+			base(pais, nombre, profundidad, tipoGrua, tipoAgua,
+				barcoCapacidad)
+		{
+			this.codigo = codigo;
+			this.ubicacion = ubicacion;
+			this.coordenadas = coordenadas;
+			this.especies = especies;
+			this.uso = uso;
+		}
+
+		//encapsulamiento a modificar
+		public DateTime Uso { get => uso; set => uso = value; }
+		public string Codigo { get => codigo; set => codigo = value; }
+		public string Ubicacion { get => ubicacion; set => ubicacion = value; }
+		public string Coordenadas { get => coordenadas; set => coordenadas = value; }
+		public string Especies { get => especies; set => especies = value; }
+	}
+}
